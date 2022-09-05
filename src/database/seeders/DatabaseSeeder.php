a218eff0eed9174d3cc090e95dac3e4f31900e3e
mar4ehk0 <mar4ehk0@gmail.com>
2022-09-05 18:26:48 +0300

    Deletes DatabaseSeeders for all modules, it is extra

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,15 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\Format\Database\Seeders\FormatsTableSeeder;
+use Modules\Genre\Database\Seeders\GenresTableSeeder;
+use Modules\GenreVideo\Database\Seeders\GenreVideosTableSeeder;
+use Modules\Person\Database\Seeders\PersonsTableSeeder;
+use Modules\PersonVideo\Database\Seeders\PersonVideoTableSeeder;
+use Modules\Position\Database\Seeders\PositionsTableSeeder;
+use Modules\Source\Database\Seeders\SourcesTableFactory;
+use Modules\Translation\Database\Seeders\TranslationsTableSeeder;
+use Modules\Video\Database\Seeders\VideosTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +23,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(FormatsTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
+        $this->call(PersonsTableSeeder::class);
+        $this->call(PositionsTableSeeder::class);
+        $this->call(TranslationsTableSeeder::class);
+        $this->call(VideosTableSeeder::class);
+        $this->call(SourcesTableFactory::class);
+        $this->call(PersonVideoTableSeeder::class);
+        $this->call(GenreVideosTableSeeder::class);
     }
 }
